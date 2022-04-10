ba0a56a10bac7e3295adad727f4595519a67b33f
Adam Olech <nddr89@gmail.com>
2022-04-10 23:40:27 +0200

    build.sh: create rootfs.cpio

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+this_path=`realpath .`
 linux_path=`realpath linux`
 busybox_path=`realpath busybox`
 
@@ -7,3 +8,6 @@ cp busybox-config $busybox_path/.config
 cd $busybox_path
 make clean
 CFLAGS="-I$linux_path/include" make install
+
+cd $this_path/build/rootfs_data
+find . | cpio -o -H newc -R root:root > ../rootfs.cpio
