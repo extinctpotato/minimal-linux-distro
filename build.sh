92cec704fa6b0f640300a5853919a0e1491a5f83
Adam Olech <nddr89@gmail.com>
2022-04-10 23:58:50 +0200

    build.sh: build linux

--- build.sh
+++ build.sh
@@ -4,10 +4,14 @@ this_path=`realpath .`
 linux_path=`realpath linux`
 busybox_path=`realpath busybox`
 
-cp busybox-config $busybox_path/.config
+cp $this_path/busybox-config $busybox_path/.config
 cd $busybox_path
 make clean
-CFLAGS="-I$linux_path/include" make install
+CFLAGS="-I$linux_path/include" make -j`nproc` install
 
 cd $this_path/build/rootfs_data
 find . | cpio -o -H newc -R root:root > ../rootfs.cpio
+
+cp $this_path/linux-config $linux_path/.config
+cd $linux_path
+make -j`nproc`
