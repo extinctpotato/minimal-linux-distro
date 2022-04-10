7e30f02c00cd20f99330ff15350c61c72c508c7c
Adam Olech <nddr89@gmail.com>
2022-04-11 00:06:35 +0200

    Improve build.sh somewhat

--- build.sh
+++ build.sh
@@ -6,12 +6,13 @@ busybox_path=`realpath busybox`
 
 cp $this_path/busybox-config $busybox_path/.config
 cd $busybox_path
-make clean
 CFLAGS="-I$linux_path/include" make -j`nproc` install
 
 cd $this_path/build/rootfs_data
+rm -rf $this_path/build/rootfs.cpio || true
 find . | cpio -o -H newc -R root:root > ../rootfs.cpio
 
 cp $this_path/linux-config $linux_path/.config
 cd $linux_path
 make -j`nproc`
+cp $linux_path/arch/x86_64/boot/bzImage $this_path/build/bzImage
